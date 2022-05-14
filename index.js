bf0fbdfc12c7251569ee6e8b1105b690f144080b
Emily81926 <emily81926@gmail.com>
2022-05-14 17:13:39 +0800

    feat: add open property when click the panel

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const panels = document.querySelectorAll('.panel')
 
+//當點擊panel時，產生open特效
+function clickedToOpen(){
+  if(this.propertyName === 'open') return
+  this.classList.toggle('open')
+}
 //在點每一個panel的時候都會加入css特效
-panels.forEach(panel => panel.addEventListener('click', () => console.log('clicked!')))
+panels.forEach(panel => panel.addEventListener('click', clickedToOpen))
